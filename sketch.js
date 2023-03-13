06e2b7d3ce6dadaf5d3af1a2ee544b97ab17c693
LucianoFilho13 <lf13112009@gmail.com>
2023-03-13 13:46:59 -0300

    first commit

---
+++ sketch.js
@@ -0,0 +1,33 @@
+// Kit Inicial Visual Studio Code
+// y0kata(Tragictx) / LucianoFilho13
+// Variaveis
+
+
+
+function preload(){ // Carregar imagens, gifs, sons/musica e videos
+
+}
+
+function setup(){ // Setar valores iniciais
+  createCanvas(400,400);
+
+}
+
+function draw(){ // Aparecer as informações na tela
+  background("black"); // Cor do fundo
+  
+
+  allFunction(); //Função de todas as funções
+  drawSprites(); //Responsavel em renderizar o jogo(sprites)
+}
+
+//Funções
+function allFunction(){
+  //Insira as funções aqui
+  function function1(){
+
+  }
+
+  //Chamar Funções
+  function1();
+}
